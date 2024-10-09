881cef3c0de4cf74f5bbf3e01fb6e4031b6d44cf
andrewzhivolup <ferari1190@mail.ru>
2024-10-09 21:16:46 +0300

    init commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import hedgehogLogo from "/hedgehog.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={hedgehogLogo} className="logo react" alt="React logo" />
+        <h1>Спаси Ежа</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
